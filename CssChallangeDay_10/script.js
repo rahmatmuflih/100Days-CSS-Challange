73faf6748a1e53dbae9913db08cb7a890850969a
rahmatmuflih <mufliharifinrahmat@gmail.com>
2024-08-12 19:08:19 +0700

    update day 9, 10 and 11

--- CssChallangeDay_10/script.js
+++ CssChallangeDay_10/script.js
@@ -1,6 +1,38 @@
 const clock = document.querySelector(".clock");
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 let point = [];
 let rotateValue = 0;
+
+function startClock() {
+  var currDate = new Date();
+  date.innerHTML = `${day[currDate.getDay()]} ${currDate.getDate()} ${
+    month[currDate.getMonth()]
+  } ${currDate.getFullYear()}`;
+  time.innerHTML = `${
+    currDate.getHours() < 10 ? "0" : ""
+  }${currDate.getHours()}<span>:</span>${
+    currDate.getMinutes() < 10 ? "0" : ""
+  }${currDate.getMinutes()}`;
+  setTimeout(startClock, 60000);
+}
+
+startClock();
 
 for (let i = 0; i < 15; i++) {
   point.push(document.createElement("div"));
