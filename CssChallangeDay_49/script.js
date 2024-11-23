35e288ca96e08b20aa7fa273e42ab25953362827
rahmatmuflih <mufliharifinrahmat@gmail.com>
2024-11-23 21:19:41 +0700

    finished day 49, update day 50

--- CssChallangeDay_49/script.js
+++ CssChallangeDay_49/script.js
@@ -1,13 +1,13 @@
 const frame = document.querySelector(".frame");
-const impact = document.querySelector(".impact");
 const circle = document.querySelector(".circle");
-let square = [];
 
-for (let i = 0; i < 400; i++) {
-  square.push(document.createElement("div"));
-  square[i].classList.add("square");
-  frame.appendChild(square[i]);
-  square[i].addEventListener("mouseover", function () {
-    circle;
-  });
-}
+frame.addEventListener("mousemove", function (e) {
+  const rect = e.target.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  circle.style.transform = `translate(${x - 10}px, ${y - 10}px)`;
+});
+
+frame.addEventListener("mouseleave", function () {
+  circle.style.transform = `translate(190px, 190px)`;
+});
